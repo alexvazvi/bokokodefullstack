6647bdf5ba0f7d819353741377a98344ff7b01be
Alex <alexvazvi98@gmail.com>
2024-01-29 14:40:17 +0100

    Cambios en Cards, product list

--- frontend/src/app/components/products-list/products-list.component.ts
+++ frontend/src/app/components/products-list/products-list.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
 
+  products = [
+    {
+      nombre: 'Producto 1',
+      precio: 20.19,
+      descripcion: 'Descripción del Producto 1'
+    },
+    {
+      nombre: 'Producto 2',
+      precio: 30.00,
+      descripcion: 'Descripción del Producto 1'
+    },
+    {
+      nombre: 'Producto 3',
+      precio: 11.19,
+      descripcion: 'Descripción del Producto 1'
+    },
+ ]
 }
