b49c1e95714d752d1a3987833c701384ee04202d
Alex <alexvazvi98@gmail.com>
2024-01-29 14:27:10 +0100

    Estructura actualizada, creacion del header, featured

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeatureProductComponent } from './components/feature-product/feature-product.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FeatureProductComponent, ProductsListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
