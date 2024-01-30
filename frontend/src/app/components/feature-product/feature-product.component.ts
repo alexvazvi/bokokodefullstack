8f1663cb31b5ec907a8e09864af6b9156b744e80
Alex <alexvazvi98@gmail.com>
2024-01-30 11:29:13 +0100

    Añadido funcionalidad cesta numero a featured

--- frontend/src/app/components/feature-product/feature-product.component.ts
+++ frontend/src/app/components/feature-product/feature-product.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../shared/card/card.component';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-feature-product',
@@ -9,5 +11,10 @@ import { CardComponent } from '../../shared/card/card.component';
   styleUrl: './feature-product.component.scss'
 })
 export class FeatureProductComponent {
+  product: Product = {name: "Perro", category: "Animal", price: 10.00}
+  constructor(private cartService: CartService) {}
 
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+  }
 }
